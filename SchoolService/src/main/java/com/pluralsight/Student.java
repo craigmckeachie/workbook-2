b6fca1deb1e1c26beacf2cbba7a035cfc5f26436
Craig McKeachie <craigdmckeachie@gmail.com>
2025-04-17 14:46:53 -0400

    classes 101.1

--- SchoolService/src/main/java/com/pluralsight/Student.java
+++ SchoolService/src/main/java/com/pluralsight/Student.java
@@ -1,35 +1,39 @@
 package com.pluralsight;
 
 public class Student {
+    //field members
     private String firstName;
     private String lastName;
-    private int age = 0;
-    private String major = "";
 
 
+    //constructor
     public Student(){
         this.firstName = "";
         this.lastName = "";
     }
 
+//this constructor not needed for exercise
+//   public Student(String firstName, String lastName){
+//      this.firstName = firstName;
+//      this.lastName = lastName;
+//   }
 
-   public Student(String firstName, String lastName){
-      this.firstName = firstName;
-      this.lastName = lastName;
-   }
-
+    //getter
    public String getFirstName(){
        return firstName;
    }
 
+   //setter
    public void setFirstName(String firstName){
         this.firstName = firstName;
    }
 
+   //getter
     public String getLastName(){
         return lastName;
     }
 
+    //setter
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
